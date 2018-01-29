f2ceb72a85cedfe8b841406a68db2890661a6818
Bela Toth <bela.toth@meltwater.com>
2018-01-29 23:01:16 +0100

    Modified STDIN handling, logger added

--- rss_link_collector.pl
+++ rss_link_collector.pl
@@ -9,6 +9,7 @@ use Data::Dumper;
 use JSON;
 use HTML::TreeBuilder::XPath;
 use WWW::Mechanize;
+use Log::Log4perl;
 use Encode;
 use constant FEED_MIME_TYPES => [
     'application/x.atom+xml',
@@ -19,55 +20,52 @@ use constant FEED_MIME_TYPES => [
     'application/rdf+xml',
 ];
 
-sub get_links {
-  my $content = shift;
+Log::Log4perl::init('log4perl.conf');
 
-  my @tags;
-
-  my $page_tree = HTML::TreeBuilder::XPath -> new_from_content(decode_utf8 $content);
-  foreach my $a_href ($page_tree -> findvalues(qq{//a/\@href})) {
-    if ($a_href =~ m/^http(s?):\/\/.*/) {
-      push(@tags, $a_href);
-    }
-  }
-
-  foreach my $link_href ($page_tree -> findvalues(qq{//link/\@href})) {
-    if ($link_href =~ m/^http(s?):\/\/.*/) {
-      push(@tags, $link_href);
-    }
-  }
-
-  return \@tags;
-}
-
+my $logger = Log::Log4perl->get_logger('house.bedrm.desk.topdrwr');
 my %IsFeed = map { $_ => 1 } @{ FEED_MIME_TYPES() };
-
 my %output_json = (
   rss  => [],
   atom => []
 );
 
-my $inputfile;
-{
-  local $/;
-  while(<>) {
-    $inputfile = $_;
+$logger->info("rss_link_collector is running!");
+my $page_tree = HTML::TreeBuilder->new();
+while (my $row = <STDIN>) {
+  $page_tree->parse($row);
+}
+$page_tree->eof();
+
+$logger->info("Input was received.");
+$logger->info("Collect the links from the input.");
+my @feed_links;
+foreach my $a_href ($page_tree -> findvalues(qq{//a/\@href})) {
+  if ($a_href =~ m/^http(s?):\/\/.*/) {
+    push(@feed_links, $a_href);
   }
 }
 
-my @feed_links = @{get_links($inputfile)};
+foreach my $link_href ($page_tree -> findvalues(qq{//link/\@href})) {
+  if ($link_href =~ m/^http(s?):\/\/.*/) {
+    push(@feed_links, $link_href);
+  }
+}
 
 foreach my $link (@feed_links) {
-  my $mech = WWW::Mechanize->new(ssl_opts => {SSL_verify_mode => "IO::Socket::SSL::SSL_VERIFY_NONE", verify_hostname => 0});
+  my $mech = WWW::Mechanize->new(autocheck => 0, ssl_opts => {SSL_verify_mode => "IO::Socket::SSL::SSL_VERIFY_NONE", verify_hostname => 0});
   my $feed_page = $mech->get($link);
 
-  if($IsFeed{$feed_page->content_type}) {
-    my $content = $feed_page->decoded_content();
-    if (index($content, "rss") > -1) {
-      push(@{$output_json{rss}}, $link);
-    } elsif (index($content, "feed") > -1) {
-      push(@{$output_json{atom}}, $link);
+  if ($mech->success()) {
+    if($IsFeed{$feed_page->content_type}) {
+      my $content = $feed_page->decoded_content();
+      if (index($content, "rss") > -1) {
+        push(@{$output_json{rss}}, $link);
+      } elsif (index($content, "feed") > -1) {
+        push(@{$output_json{atom}}, $link);
+      }
     }
+  } else {
+    $logger->error("The URL [" . $link . "] retrieves the following error: " . $mech->status());
   }
 }
 
@@ -75,4 +73,5 @@ print "\n";
 print encode_json \%output_json;
 print "\n";
 
+$logger->info("rss_link_collector finished!");
 exit 0;
